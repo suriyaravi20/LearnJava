d5a85417193233a7a49fb1915fd42e6b2a04dff7
SURIYA <SURIYA@SURIYA>
2023-05-08 18:34:31 +0530

    Initial commit

---
+++ learnJava/src/learnJava/Sample.java
@@ -0,0 +1,11 @@
+package learnJava;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("first java program");
+	}
+
+}
